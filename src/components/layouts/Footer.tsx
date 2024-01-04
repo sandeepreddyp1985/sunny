f1264723ed8ccba2c7629f4c16d7062930063ac4
sandeepreddyp1985 <sandeepreddy1985@gmail.com>
2024-01-03 20:10:28 -0600

    name change

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -29,7 +29,7 @@ export default function Footer() {
               sx={{ color: "text.primary" }}
               target="_blank"
             >
-              Crazy Man
+              Sandeep Reddy
             </Link>
           </Typography>
         </Divider>
